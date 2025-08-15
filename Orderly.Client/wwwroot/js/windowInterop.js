7ba455b5feb0f6961218ed6872e6e6d5454cdef5
bytezera <tajfoz04@gmail.com>
2025-08-15 19:23:01 +0100

    Redesigned dashboard layout and dashboard overview

---
+++ Orderly.Client/wwwroot/js/windowInterop.js
@@ -0,0 +1,11 @@
+﻿
+window.windowInterop = {
+    getWidth: function () {
+        return window.innerWidth;
+    },
+    registerResizeHandler: function (dotnetHelper) {
+        window.addEventListener("resize", () => {
+            dotnetHelper.invokeMethodAsync("UpdateWidth", window.innerWidth);
+        });
+    }
+};
