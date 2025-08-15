6671fc74b6d4b4a7c9cb1603f5adc4eaffddc0d3
bytezera <tajfoz04@gmail.com>
2025-08-15 22:20:30 +0100

    Redesigned placed and received order pages

---
+++ Orderly.Shared/Helpers/StringHelper.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orderly.Shared.Helpers
+{
+    public class StringHelper
+    {
+        public static string Truncate(string input, int maxLength, bool addEllipsis = true)
+        {
+            if (string.IsNullOrEmpty(input) || maxLength <= 0)
+            {
+                return string.Empty;
+            }
+
+            if (input.Length <= maxLength)
+            {
+                return input;
+            }
+
+            return addEllipsis && maxLength > 3
+                ? input.Substring(0, maxLength - 1) + "…"
+                : input.Substring(0, maxLength);
+        }
+    }
+}
